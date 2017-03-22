1534216765aaf6fb9df37e14194b26687a3c726a
Yekta Özdemir <lakers_yekta_ceza@hotmail.com>
2017-03-22 13:58:22 +0300

    Updated.

--- mathAverages/Source.c
+++ mathAverages/Source.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 #include<math.h>
 
+void artFunc(int arraySize, int myArray);
+
 int main()
 {
-	int arraySize = 0, getMem = 0;
-	float artMean = 0, geoMean = 1, harMean = 1;
-	int aS = 0, gS = 0, hS;
+	
+	int arraySize = 0, getMem = 0;	
 	int myArray[100];
+
 	printf("Enter The Array Size:");
 	scanf_s("%d", &arraySize);
 	printf("Enter The Array Members:");
@@ -14,31 +16,22 @@ int main()
 	{
 		scanf_s("%d", &myArray[getMem]);
 	}
+	void artFunc(arraySize, myArray);
 
-	//Aritmetic Mean
+	
+	system("PAUSE");
+	return 0;
+}
+
+void artFunc(int arraySize, int myArray)
+{
+	printf("Function is Called.\n");
+	int *aS;
+	double artMean = 0.0;
+
 	for (aS = 0; aS < arraySize; aS++)
 	{
 		artMean += myArray[aS];
 	}
-	printf("Aritmetic Mean is:%.3f\n", artMean/arraySize);
-
-	//Geometric Mean
-	for (gS = 0; gS < arraySize; gS++)
-	{
-		geoMean *= myArray[gS];
-	}
-	geoMean=sqrt(geoMean);
-	
-	printf("Geometric Mean is:%.3f\n", geoMean);
-
-	//Harmonic Mean
-	for (hS = 0; hS < arraySize; hS++)
-	{
-		harMean += 1 / myArray[hS];
-		printf("%.3f\n", harMean);
-	}
-
-	printf("Harmonic Mean is:%.3f\n", harMean);
-	system("PAUSE");
-	return 0;
+	printf("Aritmetic Mean is:%.3f\n", artMean / arraySize);
 }
